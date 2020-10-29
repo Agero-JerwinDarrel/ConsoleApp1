8d71da264dee1ffcc05ae2607ff60c2bad54caea
Nino Francisco Alamo <nin.alamo@outlook.com>
2020-10-29 16:31:39 +0800

    Removed IAnimal

--- Animal.cs
+++ Animal.cs
@@ -1,12 +1,15 @@
-﻿namespace prelim_exam
+﻿
+namespace prelim_exam
 {
-    public abstract class Animal
+    public abstract class Animal 
     {
         //TODO: Modify this class so that we can set _class ONLY ONCE 
         string _class;
+
         //TODO: Modify this class so that we can set _ofType ONLY ONCE
         //_ofType sample: Bird, Fish, Mammal -- all these classes are already provided
         string _ofType;
+
         //TODO: Modify so that we can set number of legs ONLY ONCE
         //TODO: Must expose the value of _numberOfLegs but not the variable OUTSIDE the class
         int _numberOfLegs;
@@ -19,5 +22,9 @@
         public abstract string GetClass();
         public abstract string OfType();
         public bool HasLegs() => _numberOfLegs > 0;
+
+        public abstract void Hunt();
+
+        public abstract void Sound();
     }
 }
